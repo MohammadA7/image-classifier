e6fa3c4482738314fcecfbd0c344c3303406394f
Moham <mohammad.a7@outlook.com>
2019-06-01 09:59:26 +0300

    utils.py: add load_model, process_image, predict function and update save save_model

--- utils.py
+++ utils.py
@@ -112,42 +112,80 @@ def test_model(device, test_data_loader, model):
         accuracy += torch.mean(equals.type(torch.FloatTensor))
     print("Model accuracy on test data: {:.3f}".format((accuracy/len(test_data_loader))*100) )
 
-def save_model(model, optimizer, class_to_idx, epochs, dir):
+def save_model(model, optimizer, class_to_idx, dir, arch, learning_rate, hidden_unit):
     checkpoint = {
               'state_dict': model.state_dict(),
-              'class_to_idx': class_to_idx
+              'optimizer_state_dict': optimizer.state_dict(),
+              'class_to_idx': class_to_idx,
+              'arch': arch,
+              'learning_rate': learning_rate,
+              'hidden_units': hidden_unit
               }
     path = dir + 'checkpoint.pth'
     torch.save(checkpoint, path)
     
     print(f"Model successfully saved in {path}")
 
-# def load_model(checkpoint_file, arch, learning_rate, hidden_units, device, output_classes):
-#     checkpoint = torch.load(checkpoint_file)
-    
-#     if arch == 'densenet121':
-#         model = models.densenet121(pretrained=True)
-#         input_size = 1024 
-#     elif arch == 'vgg16':
-#         model = models.vgg16(pretrained=True)
-#         input_size = 25088
-#     else:
-#         raise Exception(f'Sorry we only support densenet121 and vgg16 architectures')
+def load_model(checkpoint_file, device):
+    checkpoint = torch.load(checkpoint_file)
+    arch = checkpoint['arch']
+    class_to_idx = checkpoint['class_to_idx']
+    hidden_units = checkpoint['hidden_units']
 
-#     for param in model.parameters():
-#         param.requires_grad = False
+    if arch == 'densenet121':
+        model = models.densenet121(pretrained=True)
+        input_size = 1024 
+    elif arch == 'vgg16':
+        model = models.vgg16(pretrained=True)
+        input_size = 25088
+    else:
+        raise Exception(f'Sorry we only support densenet121 and vgg16 architectures')
 
-#     model.classifier = nn.Sequential(
-#         nn.Linear(input_size, hidden_units),
-#         nn.ReLU(),
-#         nn.Dropout(p=0.2),
-#         nn.Linear(hidden_units, output_classes),
-#         nn.LogSoftmax(dim=1))
+    model.classifier = nn.Sequential(
+        nn.Linear(input_size, hidden_units),
+        nn.ReLU(),
+        nn.Dropout(p=0.2),
+        nn.Linear(hidden_units, len(checkpoint['class_to_idx'])),
+        nn.LogSoftmax(dim=1))
     
-#     model.load_state_dict(checkpoint['state_dict'])
-    
-#     optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
-#     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-#     model.to(device) 
+    model.load_state_dict(checkpoint['state_dict'])
+    model.to(device)
 
-#     return model, optimizer
+    return model, class_to_idx
+
+def process_image(image, device):
+    image.thumbnail((256,256))
+    width, height = image.size
+
+    left = (width - 224)/2
+    top = (height - 224)/2
+    right = (width + 224)/2
+    bottom = (height + 224)/2
+    
+    croped_image = image.crop((left, top, right, bottom))
+    np_image = np.array(croped_image)
+    np_image = np_image/ 255
+    
+    means = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    np_image = (np_image - means) / std
+    
+    transpose_image = np_image.transpose((2,0,1))
+    
+    return torch.from_numpy(transpose_image).to(device)
+
+
+def predict(image_path, model, topk, device, idx_to_class, process_image):
+    image = Image.open(image_path)
+    
+    preprocess_image = process_image(image, device)
+    preprocess_image = preprocess_image.unsqueeze_(0)
+    preprocess_image = preprocess_image.float()
+    
+    model.to(device)
+
+    log_ps = model(preprocess_image)
+    ps = torch.exp(log_ps)
+    top_p, top_index = ps.topk(topk, dim=1)
+    top_class = [idx_to_class[index] for index in top_index[0].tolist()] 
+    return top_p, top_class
